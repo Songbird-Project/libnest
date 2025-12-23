c68ba211e4b66eb71619436cd3393150a53fa031
vaelixd <vaelixd@proton.me>
2025-12-24 10:40:08 +1300

    add `root.zig`

--- src/root.zig
+++ src/root.zig
@@ -1,1 +1,6 @@
+//! libnest is a package management library designed for Nest.
+
 const std = @import("std");
+const errors = @import("errors.zig");
+const curl = @import("curl");
+const handle = @import("handle.zig");
