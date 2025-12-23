50952d049666394a7ec43b6b3189cdc31d2557a2
vaelixd <vaelixd@proton.me>
2025-12-24 11:02:26 +1300

    update to use new handle

--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,6 @@
 //! libnest is a package management library designed for Nest.
 
 const std = @import("std");
-const errors = @import("errors.zig");
+const errors = @import("error.zig");
 const curl = @import("curl");
-const handle = @import("handle.zig");
+const Handle = @import("Handle.zig");
--- src/tests.zig
+++ src/tests.zig
@@ -1,11 +1,11 @@
 const std = @import("std");
 const testing = std.testing;
-const handle = @import("handle.zig");
+const Handle = @import("Handle.zig");
 const curl = @import("curl");
 
 test "handle memory" {
     const alloc = std.testing.allocator;
 
-    const hndl = try handle.nestHandleNew(alloc);
-    handle.nestHandleFree(alloc, hndl);
+    const hndl = try Handle.init(alloc);
+    hndl.deinit(alloc);
 }
