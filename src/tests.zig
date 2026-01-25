80dd0aeff22f4117f513539722aea1c97330b16a
vaelixd <vaelixd@proton.me>
2026-01-25 18:36:35 +1300

    feat(testing): add test for downloading via MirrorList

---
+++ src/tests.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+
+const Downloader = @import("net/Downloader.zig");
+const MirrorList = @import("net/MirrorList.zig");
+const Pkg = @import("core/Package.zig");
+
+test "download" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const alloc = gpa.allocator();
+
+    var mirrors = try MirrorList.init(alloc, "./mirrors");
+    defer mirrors.deinit();
+
+    var flex = try Pkg.init(alloc, "flex", "2.6.4-5");
+    defer flex.deinit();
+    flex.arch = try alloc.dupe(u8, "x86_64");
+    flex.repo = try alloc.dupe(u8, "core");
+
+    try mirrors.download(flex, "./flex.pkg.tar.zst", &cb);
+}
+
+fn cb(dlnow: f64, dltotal: f64) !void {
+    const bar_width: usize = 10;
+    const filled: u8 = @min(bar_width, @as(u8, @intFromFloat((dlnow / dltotal) * 10)));
+
+    var bar: [bar_width]u8 = undefined;
+    @memset(bar[0..filled], '#');
+    @memset(bar[filled..], ' ');
+
+    std.debug.print("\r[{s}]", .{bar});
+}
