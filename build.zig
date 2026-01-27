6a988f177b16747fb60239b3e60036d48a772190
vaelixd <vaelixd@proton.me>
2026-01-28 07:45:01 +1300

    refactor(build)!: allow for both static and dynamic libraries to be emitted in one build
    Instead of the following:
    `zig build -Demit-lib`
    
    Use:
    `zig build -Demit-static`
    
    `-Ddynamic` has been replaced with `-Demit-dynamic` and can act by
    itself.

--- build.zig
+++ build.zig
@@ -2,8 +2,17 @@ const std = @import("std");
 const builtin = @import("builtin");
 
 pub fn build(b: *std.Build) void {
-    const emit_lib = b.option(bool, "emit-lib", "Emit a static library file (use -Ddynamic for a dynamic library)");
-    const dynamic = b.option(bool, "dynamic", "Emit a dynamic library");
+    const emit_static = b.option(
+        bool,
+        "emit-static",
+        "Emit a static library",
+    ) orelse false;
+    const emit_dynamic = b.option(
+        bool,
+        "emit-dynamic",
+        "Emit a dynamic library",
+    ) orelse false;
+
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
@@ -27,12 +36,6 @@ pub fn build(b: *std.Build) void {
 
     const curl = b.dependency("curl", .{ .link_vendor = false });
     module.addImport("curl", curl.module("curl"));
-    module.addLibraryPath(.{
-        .dependency = .{
-            .dependency = curl,
-            .sub_path = "/usr/lib/",
-        },
-    });
 
     const tests = b.addTest(.{
         .root_module = b.addModule("tests", .{
@@ -44,6 +47,7 @@ pub fn build(b: *std.Build) void {
     });
 
     tests.linkSystemLibrary("curl");
+    tests.linkSystemLibrary("archive");
     tests.linkLibC();
     tests.root_module.addImport("curl", curl.module("curl"));
     tests.root_module.addImport("sqlite", sqlite.module("sqlite"));
@@ -52,34 +56,35 @@ pub fn build(b: *std.Build) void {
 
     const test_step = b.step("test", "Run libnest tests");
     test_step.dependOn(&run_tests.step);
-    b.getInstallStep().dependOn(test_step);
 
-    if ((emit_lib != null and emit_lib.?) or (dynamic != null and dynamic.?)) {
-        var lib: ?*std.Build.Step.Compile = null;
+    if (emit_dynamic) {
+        const lib = b.addLibrary(.{
+            .name = "nest",
+            .root_module = module,
+            .linkage = .dynamic,
+            .version = .{
+                .major = 0,
+                .minor = 1,
+                .patch = 0,
+            },
+        });
 
-        if (dynamic != null and dynamic.?) {
-            lib = b.addLibrary(.{
-                .name = "nest",
-                .root_module = module,
-                .linkage = .dynamic,
-                .version = .{
-                    .major = 0,
-                    .minor = 1,
-                    .patch = 0,
-                },
-            });
-        } else {
-            lib = b.addLibrary(.{
-                .name = "nest",
-                .root_module = module,
-                .linkage = .static,
-            });
-        }
+        lib.linkSystemLibrary("curl");
+        lib.linkSystemLibrary("archive");
+        lib.linkLibC();
+        b.installArtifact(lib);
+    }
 
-        if (lib) |l| {
-            l.linkSystemLibrary("curl");
-            l.linkLibC();
-            b.installArtifact(l);
-        }
+    if (emit_static) {
+        const lib = b.addLibrary(.{
+            .name = "nest",
+            .root_module = module,
+            .linkage = .static,
+        });
+
+        lib.linkSystemLibrary("curl");
+        lib.linkSystemLibrary("archive");
+        lib.linkLibC();
+        b.installArtifact(lib);
     }
 }
