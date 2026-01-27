f3495d0953dabf08410158160f957a31bdff1b83
vaelixd <vaelixd@proton.me>
2026-01-28 10:12:13 +1300

    feat(parse): take in a kind for indexing, either `Path` or `Buf`

--- src/parse/desc.zig
+++ src/parse/desc.zig
@@ -2,8 +2,17 @@ const std = @import("std");
 
 const Db = @import("../core/Database.zig");
 
-pub fn index(alloc: std.mem.Allocator, db: *Db, path: []const u8, repo: []const u8) !void {
-    var fields: std.StringHashMap([]const u8) = try parse(alloc, path);
+pub const descKind = enum {
+    Path,
+    Buf,
+};
+
+pub fn index(alloc: std.mem.Allocator, db: *Db, desc: []const u8, repo: []const u8, kind: descKind) !void {
+    var fields: std.StringHashMap([]const u8) = if (kind == .Path)
+        try parse(alloc, desc)
+    else
+        try parseString(alloc, desc);
+
     defer {
         var it = fields.keyIterator();
         while (it.next()) |key| alloc.free(key.*);
@@ -151,6 +160,79 @@ pub fn parse(alloc: std.mem.Allocator, path: []const u8) !std.StringHashMap([]co
 
         _ = reader.toss(1);
         line.clearRetainingCapacity();
+    }
+
+    if (current_field) |field| {
+        if (current_value.items[0] == '[') {
+            try current_value.insert(alloc, current_value.items.len - 1, ']');
+            _ = current_value.pop();
+        }
+
+        try fields.put(
+            field,
+            try current_value.toOwnedSlice(alloc),
+        );
+        current_value.clearRetainingCapacity();
+    }
+
+    return fields;
+}
+
+pub fn parseString(alloc: std.mem.Allocator, src: []const u8) !std.StringHashMap([]const u8) {
+    var lines = std.mem.splitScalar(u8, src, '\n');
+
+    var current_field: ?[]const u8 = null;
+    var fields: std.StringHashMap([]const u8) = .init(alloc);
+    errdefer {
+        if (current_field) |field| alloc.free(field);
+        var it = fields.keyIterator();
+        while (it.next()) |key| alloc.free(key.*);
+        it = fields.valueIterator();
+        while (it.next()) |value| alloc.free(value.*);
+
+        fields.deinit();
+    }
+
+    var current_value: std.ArrayList(u8) = .empty;
+    defer current_value.deinit(alloc);
+
+    while (lines.next()) |line| {
+        const trimmed_line = std.mem.trim(
+            u8,
+            line,
+            " \r\t",
+        );
+
+        if (trimmed_line.len > 0) {
+            if (trimmed_line.len > 2 and trimmed_line[0] == '%' and trimmed_line[trimmed_line.len - 1] == '%') {
+                if (current_field) |field| {
+                    if (current_value.items.len > 0) {
+                        if (current_value.items[0] == '[') {
+                            try current_value.insert(alloc, current_value.items.len - 1, ']');
+                            _ = current_value.pop();
+                        }
+                    }
+
+                    try fields.put(
+                        field,
+                        try current_value.toOwnedSlice(alloc),
+                    );
+                    current_value.clearRetainingCapacity();
+                }
+
+                current_field = null;
+                current_field = try alloc.dupe(u8, trimmed_line[1 .. trimmed_line.len - 1]);
+            } else if (current_field) |_| {
+                if (current_value.items.len > 0) {
+                    if (current_value.items[0] != '[')
+                        try current_value.insert(alloc, 0, '[');
+
+                    try current_value.append(alloc, ',');
+                }
+
+                try current_value.appendSlice(alloc, trimmed_line);
+            }
+        }
     }
 
     if (current_field) |field| {
