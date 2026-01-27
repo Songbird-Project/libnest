ebce2f106c8b12e3f46f67ba38146fff7f4d9dd9
vaelixd <vaelixd@proton.me>
2026-01-28 09:02:11 +1300

    test: use constants for reptitive values
    Tests now use the `test` type for commits to avoid them beig classed as
    minor releases or patches.

--- src/tests.zig
+++ src/tests.zig
@@ -7,6 +7,9 @@ const Db = @import("core/Database.zig");
 
 const desc = @import("parse/desc.zig");
 
+const PKG_DB: []const u8 = "./tests/pkgs.db";
+const MIRRORS: []const u8 = "./tests/mirrors";
+
 fn cb(dlnow: f64, dltotal: f64) !void {
     const bar_width: usize = 10;
     const filled: u8 = @min(bar_width, @as(u8, @intFromFloat((dlnow / dltotal) * 10)));
@@ -18,36 +21,16 @@ fn cb(dlnow: f64, dltotal: f64) !void {
     std.debug.print("\r[{s}]", .{bar});
 }
 
-test "Package Download" {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const alloc = gpa.allocator();
-
-    var db = try Db.init(alloc, "./tests/core.db");
-    defer db.deinit();
-
-    var mirrors = try MirrorList.init(alloc, "./tests/mirrors");
-    defer mirrors.deinit();
-
-    try mirrors.downloadPkg(
-        &db,
-        "binutils",
-        "core",
-        "./tests/binutils.pkg.tar.zst",
-        &cb,
-    );
-}
-
 test "Sync Mirrors" {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const alloc = gpa.allocator();
 
-    var db = try Db.init(alloc, "./tests/core.db");
+    var db = try Db.init(alloc, PKG_DB);
     defer db.deinit();
 
     try db.sync(
-        "./tests/mirrors",
+        MIRRORS,
         "./tests/",
         &[_][]const u8{"core"},
         "x86_64",
@@ -60,8 +43,28 @@ test "DB Indexing" {
     defer _ = gpa.deinit();
     const alloc = gpa.allocator();
 
-    var db = try Db.init(alloc, "./tests/core.db");
+    var db = try Db.init(alloc, PKG_DB);
     defer db.deinit();
 
     try desc.index(alloc, &db, "./tests/desc", "core");
+}
+
+test "Package Download" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const alloc = gpa.allocator();
+
+    var db = try Db.init(alloc, PKG_DB);
+    defer db.deinit();
+
+    var mirrors = try MirrorList.init(alloc, MIRRORS);
+    defer mirrors.deinit();
+
+    try mirrors.downloadPkg(
+        &db,
+        "binutils",
+        "core",
+        "./tests/binutils.pkg.tar.zst",
+        &cb,
+    );
 }
