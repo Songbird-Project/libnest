5d916375471f4a115599766432620ce408b290ed
vaelixd <vaelixd@proton.me>
2026-02-01 15:54:14 +1300

    feat: add top-level exports to `libnest`

--- src/root.zig
+++ src/root.zig
@@ -1,4 +1,15 @@
 const std = @import("std");
 
-pub const Database = @import("core/Database.zig");
-pub const Package = @import("core/Package.zig");
+pub const Db = @import("core/Database.zig");
+pub const Pkg = @import("core/Package.zig");
+
+pub const net = struct {
+    pub const Downloader = @import("net/Downloader.zig");
+    pub const MirrorList = @import("net/MirrorList.zig");
+};
+
+pub const version = std.SemanticVersion{
+    .major = 0,
+    .minor = 1,
+    .patch = 0,
+};
