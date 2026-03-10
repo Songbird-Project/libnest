ea398106694853983108c1b8a2931216de2c8655
vaelixd <vaelixd@proton.me>
2026-03-10 23:06:45 +1300

    feat: expose more of the api

--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,10 @@
 const std = @import("std");
 
+pub const mdb = @import("utils/mdb.zig");
+pub const version = @import("core/version.zig");
+
 pub const Db = @import("core/Database.zig");
+pub const Dependency = @import("core/Dependency.zig");
 pub const Pkg = @import("core/Package.zig");
 
 pub const net = struct {
@@ -8,7 +12,13 @@ pub const net = struct {
     pub const MirrorList = @import("net/MirrorList.zig");
 };
 
-pub const version = std.SemanticVersion{
+pub const AUR = struct {
+    pub const Builder = @import("aur/Builder.zig");
+    pub const Client = @import("aur/Client.zig");
+    pub const Pkg = @import("aur/Package.zig");
+};
+
+pub const libnest_version = std.SemanticVersion{
     .major = 0,
     .minor = 1,
     .patch = 0,
