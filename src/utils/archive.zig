53cf9f31101b9fb083aa0fb85e2c60131b60a09e
vaelixd <vaelixd@proton.me>
2026-03-10 23:03:18 +1300

    feat: add `Writer` add error union

--- src/utils/archive.zig
+++ src/utils/archive.zig
@@ -4,10 +4,63 @@ pub const c = @cImport({
     @cInclude("archive_entry.h");
 });
 
+pub const ArchiveError = error{
+    UnableToCreateReader,
+    UnableToCreateWriter,
+    WriteHeaderFailed,
+    OpenFailed,
+    ReadFailed,
+};
+
+pub const Writer = struct {
+    writer: *c.struct_archive,
+
+    pub fn init() ArchiveError!Writer {
+        const writer = c.archive_write_disk_new() orelse
+            return error.UnableToCreateWriter;
+
+        _ = c.archive_write_disk_set_options(
+            writer,
+            c.ARCHIVE_EXTRACT_PERM |
+                c.ARCHIVE_EXTRACT_TIME |
+                c.ARCHIVE_EXTRACT_OWNER |
+                c.ARCHIVE_EXTRACT_ACL |
+                c.ARCHIVE_EXTRACT_SECURE_NODOTDOT |
+                c.ARCHIVE_EXTRACT_SECURE_SYMLINKS |
+                c.ARCHIVE_EXTRACT_UNLINK |
+                c.ARCHIVE_EXTRACT_FFLAGS,
+        );
+
+        return .{ .writer = writer };
+    }
+
+    pub fn deinit(self: *Writer) void {
+        _ = c.archive_write_free(self.writer);
+    }
+
+    pub fn writeHeader(self: *Writer, entry: *c.archive_entry, path: []const u8) ArchiveError!void {
+        c.archive_entry_set_pathname(entry, path.ptr);
+        const ret = c.archive_write_header(self.writer, entry);
+        if (ret != c.ARCHIVE_OK) return error.WriteHeaderFailed;
+    }
+
+    pub fn writeData(self: *Writer, data: []const u8, bytes: usize) ArchiveError!void {
+        _ = c.archive_write_data(
+            self.writer,
+            data.ptr,
+            bytes,
+        );
+    }
+
+    pub fn finishEntry(self: *Writer) ArchiveError!void {
+        _ = c.archive_write_finish_entry(self.writer);
+    }
+};
+
 pub const Reader = struct {
     archive: *c.struct_archive,
 
-    pub fn init() !Reader {
+    pub fn init() ArchiveError!Reader {
         const archive = c.archive_read_new() orelse
             return error.UnableToCreateReader;
 
