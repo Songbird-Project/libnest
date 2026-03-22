2764b244c35782ed3b457b9ac9742c0e4d858db2
vaelixd <vaelixd@proton.me>
2026-03-23 09:54:34 +1300

    feat: add makepkg path to `Builder` config and run makepkg

--- src/aur/Builder.zig
+++ src/aur/Builder.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 const Pkg = @import("./Package.zig");
 const git = @import("../utils/git.zig");
@@ -6,15 +7,17 @@ const git = @import("../utils/git.zig");
 const Builder = @This();
 
 alloc: std.mem.Allocator,
+makepkg_path: []const u8,
 
-pub fn init(alloc: std.mem.Allocator) !Builder {
+pub fn init(alloc: std.mem.Allocator, makepkg_path: []const u8) !Builder {
     return .{
         .alloc = alloc,
+        .makepkg_path = try alloc.dupe(u8, makepkg_path),
     };
 }
 
 pub fn deinit(self: *Builder) void {
-    _ = self;
+    self.alloc.free(self.makepkg_path);
 }
 
 pub fn build(self: *Builder, prefix: ?[]const u8, pkg: Pkg.Basic) !void {
@@ -27,7 +30,6 @@ pub fn build(self: *Builder, prefix: ?[]const u8, pkg: Pkg.Basic) !void {
     defer self.alloc.free(cache);
 
     _ = git.init();
-    defer _ = git.deinit();
 
     const url = try std.fmt.allocPrint(
         self.alloc,
@@ -41,5 +43,29 @@ pub fn build(self: *Builder, prefix: ?[]const u8, pkg: Pkg.Basic) !void {
         url,
         cache,
     );
-    defer git.free_repository(repo);
+
+    git.free_repository(repo);
+    _ = git.deinit();
+
+    var child = std.process.Child.init(&.{
+        self.makepkg_path,
+    }, self.alloc);
+
+    child.stdin_behavior = .Ignore;
+    child.stdout_behavior = if (builtin.is_test) .Ignore else .Inherit;
+    child.stderr_behavior = if (builtin.is_test) .Ignore else .Inherit;
+    child.cwd = cache;
+
+    const term = try child.spawnAndWait();
+
+    switch (term) {
+        .Exited => |code| {
+            switch (code) {
+                0 => return,
+                13 => return error.AlreadyBuilt,
+                else => return error.BuildFailed,
+            }
+        },
+        else => return error.ProcessTerminatedUnexpectedly,
+    }
 }
