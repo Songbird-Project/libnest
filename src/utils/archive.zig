b5fe8aae6a1d16a69d787ddf1ac59f979d5ac2fe
vaelixd <vaelixd@proton.me>
2026-05-09 17:14:41 +1200

    fix: hardlink path resolution and error output

--- src/utils/archive.zig
+++ src/utils/archive.zig
@@ -1,13 +1,17 @@
 const std = @import("std");
+const installer = @import("../core/installer.zig");
 pub const c = @cImport({
     @cInclude("archive.h");
     @cInclude("archive_entry.h");
 });
 
+const Context = @import("../core/Context.zig");
+
 pub const ArchiveError = error{
     UnableToCreateReader,
     UnableToCreateWriter,
     WriteHeaderFailed,
+    WriteFailed,
     OpenFailed,
     ReadFailed,
 };
@@ -27,6 +31,7 @@ pub const Writer = struct {
                 c.ARCHIVE_EXTRACT_ACL |
                 c.ARCHIVE_EXTRACT_SECURE_NODOTDOT |
                 c.ARCHIVE_EXTRACT_SECURE_SYMLINKS |
+                c.ARCHIVE_EXTRACT_SAFE_WRITES |
                 c.ARCHIVE_EXTRACT_UNLINK |
                 c.ARCHIVE_EXTRACT_FFLAGS,
         );
@@ -40,11 +45,52 @@ pub const Writer = struct {
 
     pub fn writeHeader(
         self: *Writer,
+        ctx: *Context,
+        info: installer.PkgInstallInfo,
         entry: *c.archive_entry,
         path: []const u8,
-    ) ArchiveError!void {
-        c.archive_entry_set_pathname(entry, path.ptr);
-        const ret = c.archive_write_header(self.writer, entry);
+    ) !void {
+        const cpath = try ctx.alloc.dupeZ(u8, path);
+        defer ctx.alloc.free(cpath);
+
+        const cloned = c.archive_entry_clone(entry) orelse
+            return error.OutOfMemory;
+        defer c.archive_entry_free(cloned);
+
+        c.archive_entry_set_pathname(cloned, cpath.ptr);
+
+        if (c.archive_entry_hardlink(cloned)) |target_ptr| {
+            const target: []const u8 = std.mem.span(target_ptr);
+
+            var rel = target;
+            if (std.mem.startsWith(u8, path, "./")) rel = rel[2..];
+            if (std.mem.startsWith(u8, path, "/")) rel = rel[1..];
+
+            const basename = std.fs.path.basename(rel);
+            const target_path = if (basename.len > 0 and basename[0] == '.')
+                try std.fs.path.join(ctx.alloc, &.{
+                    info.cache,
+                    rel,
+                })
+            else if (std.mem.startsWith(u8, path, "/usr/share/libalpm/hooks/"))
+                try std.fs.path.join(ctx.alloc, &.{
+                    ctx.paths.hook,
+                    rel[25..],
+                })
+            else
+                try std.fs.path.join(ctx.alloc, &.{
+                    ctx.paths.root,
+                    rel,
+                });
+            defer ctx.alloc.free(target_path);
+
+            const c_target_path = try ctx.alloc.dupeZ(u8, target_path);
+            defer ctx.alloc.free(c_target_path);
+
+            c.archive_entry_set_hardlink(cloned, c_target_path.ptr);
+        }
+
+        const ret = c.archive_write_header(self.writer, cloned);
         if (ret != c.ARCHIVE_OK) {
             std.debug.print("{s}\n", .{c.archive_error_string(self.writer)});
             return error.WriteHeaderFailed;
@@ -56,11 +102,16 @@ pub const Writer = struct {
         data: []const u8,
         bytes: usize,
     ) ArchiveError!void {
-        _ = c.archive_write_data(
+        const ret = c.archive_write_data(
             self.writer,
             data.ptr,
             bytes,
         );
+
+        if (ret < 0) {
+            std.debug.print("{s}\n", .{c.archive_error_string(self.writer)});
+            return error.WriteFailed;
+        }
     }
 
     pub fn finishEntry(self: *Writer) ArchiveError!void {
