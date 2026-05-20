d54768645bcc66eba66a8c358a621dc25a168faf
vaelixd <vaelixd@proton.me>
2026-05-20 13:01:12 +1200

    feat: expose all APIs

--- src/root.zig
+++ src/root.zig
@@ -10,6 +10,7 @@ pub const Context = @import("core/Context.zig");
 pub const Db = @import("core/Database.zig");
 pub const Dependency = @import("core/Dependency.zig");
 pub const Pkg = @import("core/Package.zig");
+pub const Txn = @import("core/Transaction.zig");
 
 pub const net = struct {
     pub const Downloader = @import("net/Downloader.zig");
@@ -20,6 +21,17 @@ pub const AUR = struct {
     pub const Builder = @import("aur/Builder.zig");
     pub const Client = @import("aur/Client.zig");
     pub const Pkg = @import("aur/Package.zig");
+};
+
+pub const Internal = struct {
+    pub const Parse = struct {
+        pub const desc = @import("parse/desc.zig");
+        pub const pkginfo = @import("parse/pkginfo.zig");
+        pub const pkgbuild = @import("parse/pkgbuild.zig");
+    };
+
+    pub const git = @import("utils/git.zig");
+    pub const archive = @import("utils/archive.zig");
 };
 
 pub const libnest_version = std.SemanticVersion{
