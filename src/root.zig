66bc69b63e893fa338944b4f016b3f6377b405b4
vaelixd <vaelixd@proton.me>
2026-07-21 11:01:29 +1200

    fix!: remove old exports

--- src/root.zig
+++ src/root.zig
@@ -1,27 +1,15 @@
 const std = @import("std");
 
+pub const config = @import("core/config.zig").config;
 pub const version = @import("core/version.zig");
-pub const installer = @import("core/installer.zig");
-pub const remover = @import("core/remover.zig");
-pub const upgrader = @import("core/upgrader.zig");
-pub const resolver = @import("core/resolver.zig");
-pub const hooks = @import("core/hooks.zig");
-
-pub const Context = @import("core/Context.zig");
-pub const Db = @import("core/Database.zig");
-pub const Dependency = @import("core/Dependency.zig");
-pub const Pkg = @import("core/Package.zig");
-pub const Txn = @import("core/Transaction.zig");
+pub const package = @import("core/package.zig");
+pub const store = @import("core/store.zig");
+pub const database = @import("core/database.zig");
+pub const sync = @import("core/sync.zig");
+pub const context = @import("core/context.zig");
 
 pub const net = struct {
-    pub const Downloader = @import("net/Downloader.zig");
-    pub const MirrorList = @import("net/MirrorList.zig");
-};
-
-pub const AUR = struct {
-    pub const Builder = @import("aur/Builder.zig");
-    pub const Client = @import("aur/Client.zig");
-    pub const Pkg = @import("aur/Package.zig");
+    pub const download = @import("net/download.zig");
 };
 
 pub const Internal = struct {
