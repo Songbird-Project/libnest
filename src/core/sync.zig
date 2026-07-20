47bc61f1e58ab353e24cc67b8620ed34c5289d5d
vaelixd <vaelixd@proton.me>
2026-07-21 10:58:42 +1200

    feat!: reimplement syncing repos

---
+++ src/core/sync.zig
@@ -0,0 +1,154 @@
+const std = @import("std");
+const Repo = @import("repo.zig").Repo;
+const download = @import("../net/download.zig");
+const Context = @import("context.zig").Context;
+const zqlite = @import("zqlite");
+const archive = @import("../utils/archive.zig");
+const desc = @import("../parse/desc.zig");
+const package = @import("package.zig");
+
+pub fn syncRepo(ctx: Context, db: zqlite.Conn, repo: Repo) !void {
+    var client = try download.CurlClient.init(ctx);
+    defer client.deinit(ctx);
+
+    const db_name = try std.fmt.allocPrint(
+        ctx.alloc,
+        "{s}-{s}.db",
+        .{ repo.name, repo.arch },
+    );
+    defer ctx.alloc.free(db_name);
+    const dest = try std.Io.Dir.path.join(ctx.alloc, &.{
+        "/tmp",
+        "db",
+        db_name,
+    });
+    defer ctx.alloc.free(dest);
+
+    for (repo.mirrors) |mirror| {
+        const repo_url = try std.mem.replaceOwned(
+            u8,
+            ctx.alloc,
+            mirror,
+            "$repo",
+            repo.name,
+        );
+        defer ctx.alloc.free(repo_url);
+
+        const resolved_url = try std.mem.replaceOwned(
+            u8,
+            ctx.alloc,
+            repo_url,
+            "$arch",
+            repo.arch,
+        );
+
+        const url = try std.fmt.allocPrint(
+            ctx.alloc,
+            "{s}/{s}.db",
+            .{ resolved_url, repo.name },
+        );
+        defer ctx.alloc.free(url);
+
+        client.downloadToFile(ctx, url, dest) catch continue;
+        break;
+    }
+
+    var reader = try archive.Reader.init();
+    defer reader.deinit();
+
+    const db_file = try std.Io.Dir.cwd().openFile(ctx.io, dest, .{});
+    defer db_file.close(ctx.io);
+
+    try reader.openFd(db_file.handle);
+    var buf: [8192]u8 = undefined;
+
+    const sync_stmt = try db.prepare(
+        \\INSERT INTO packages(name, epoch, version, release, checksum)
+        \\VALUES (?1, ?2, ?3, ?4, ?5)
+        \\ON CONFLICT(name) DO UPDATE SET
+        \\  epoch = excluded.epoch,
+        \\  version = excluded.version,
+        \\  release = excluded.release,
+        \\  checksum = excluded.checksum
+        \\WHERE vercmp(excluded.epoch, excluded.version, excluded.release,
+        \\             packages.epoch, packages.version, packages.release) > 0
+        \\RETURNING id;
+    );
+
+    try db.transaction();
+
+    while (try reader.nextEntry()) |entry| {
+        const path: []const u8 = std.mem.span(archive.c.archive_entry_pathname(entry));
+        if (!std.mem.eql(u8, std.Io.Dir.path.basename(path), "desc")) continue;
+
+        try db.execNoArgs("SAVEPOINT pkg");
+        errdefer db.execNoArgs("ROLLBACK TO pkg") catch {};
+
+        var contents: std.ArrayList(u8) = .empty;
+        defer contents.deinit(ctx.alloc);
+
+        while (true) {
+            const read = try reader.readData(&buf);
+            if (read <= 0) break;
+            try contents.appendSlice(ctx.alloc, buf[0..read]);
+        }
+
+        const pkg_info = try desc.parse(
+            ctx.alloc,
+            repo.name,
+            contents.items,
+        );
+        defer pkg_info.deinit(ctx.alloc);
+
+        try sync_stmt.bind(.{
+            pkg_info.name,
+            pkg_info.epoch,
+            pkg_info.version,
+            pkg_info.release,
+            pkg_info.checksum,
+        });
+        const row = try sync_stmt.step();
+
+        if (row) {
+            const id = try sync_stmt.int(0);
+
+            try db.exec(
+                \\DELETE FROM depends WHERE package_id = ?1;
+                \\DELETE FROM provides WHERE package_id = ?1;
+                \\DELETE FROM conflicts WHERE package_id = ?1;
+                \\DELETE FROM replaces WHERE package_id = ?1;
+                \\DELETE FROM licenses WHERE package_id = ?1;
+            , .{id});
+            for (pkg_info.deps) |dep| {
+                try db.exec(
+                    \\INSERT INTO depends(package_id, name, kind, constraint) VALUES (?1, ?2, ?3, ?4)
+                , .{ id, dep.name, dep.kind, dep.constraint });
+            }
+            for (pkg_info.provides) |provide| {
+                try db.exec(
+                    \\INSERT INTO provides(package_id, name, ver_constraint) VALUES (?1, ?2, ?3)
+                , .{ id, provide.name, provide.constraint });
+            }
+
+            for (pkg_info.conflicts) |confs| {
+                try db.exec(
+                    \\INSERT INTO conflicts(package_id, name, ver_constraint) VALUES (?1, ?2, ?3)
+                , .{ id, confs.name, confs.constraint });
+            }
+            for (pkg_info.replaces) |reps| {
+                try db.exec(
+                    \\INSERT INTO replaces(package_id, name, ver_constraint) VALUES (?1, ?2, ?3)
+                , .{ id, reps.name, reps.constraint });
+            }
+            for (pkg_info.licenses) |name| {
+                try db.exec(
+                    \\INSERT INTO licenses(package_id, name) VALUES (?1, ?2)
+                , .{ id, name });
+            }
+        }
+
+        sync_stmt.reset();
+        try db.execNoArgs("RELEASE pkg");
+    }
+    try db.commit();
+}
