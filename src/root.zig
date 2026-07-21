5d0a4066b5d1ea63b9507c3b94fdfc8ed480594b
vaelixd <vaelixd@proton.me>
2026-07-21 21:36:39 +1200

    fix: add `repo` to public api

--- src/root.zig
+++ src/root.zig
@@ -1,12 +1,12 @@
 const std = @import("std");
 
 pub const config = @import("core/config.zig").config;
-pub const version = @import("core/version.zig");
 pub const package = @import("core/package.zig");
 pub const store = @import("core/store.zig");
 pub const database = @import("core/database.zig");
 pub const sync = @import("core/sync.zig");
 pub const context = @import("core/context.zig");
+pub const repo = @import("core/repo.zig");
 
 pub const net = struct {
     pub const download = @import("net/download.zig");
@@ -15,13 +15,14 @@ pub const net = struct {
 pub const Internal = struct {
     pub const Parse = struct {
         pub const desc = @import("parse/desc.zig");
-        pub const pkginfo = @import("parse/pkginfo.zig");
-        pub const pkgbuild = @import("parse/pkgbuild.zig");
+        // pub const pkginfo = @import("parse/pkginfo.zig");
+        // pub const pkgbuild = @import("parse/pkgbuild.zig");
     };
 
     pub const git = @import("utils/git.zig");
     pub const archive = @import("utils/archive.zig");
     pub const mem = @import("utils/mem.zig");
+    pub const version = @import("utils/version.zig");
 };
 
 pub const libnest_version = std.SemanticVersion{
